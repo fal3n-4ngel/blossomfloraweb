18f477c341b33b41e05796f886e83d5f9d0ebb86
Adithya Krishnan <adiadithyakrishnan@gmail.com>
2023-08-29 22:15:42 +0530

    update : home

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,22 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import localFont from '@next/font/local'
+
+const logo = localFont({
+  src: [
+    {
+      path: '../../public/fonts/Great Bromwich W01 Bold.ttf',
+      weight: '400'
+    },
+    {
+      path: '../../public/fonts/Great Bromwich W01 Bold.ttf',
+      weight: '700'
+    }
+  ],
+  variable: '--font-logo'
+})
+
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,7 +31,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className={`${logo.variable} font-sans`}>
       <body className={inter.className}>{children}</body>
     </html>
   )
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,9 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      fontFamily:{
+        logo: ['var(--font-logo)']
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
